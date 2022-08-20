0fd9eb9772b3afb727258334aec53fbdf8696599
PascalDupontBrussels <52561319+PascalDupontBrussels@users.noreply.github.com>
2022-08-20 13:42:32 +0200

    bluebank.py (This is my script for Blue Bank Loan Analysis)
    This is my script for Blue Bank Loan Analysis - this one done on Python.

--- Python_Tableau_Analysis/bluebank.py
+++ Python_Tableau_Analysis/bluebank.py
@@ -54,7 +54,6 @@ arr = np.array([1, 2, 3, 4])
 arr = np.array([[1,2,3], [4,5,6]])
 
 #working with IF statements
-
 a = 40
 b = 500
 c = 30
@@ -90,9 +89,7 @@ else:
 print(ficocat)
     
 #applying for loops to loandata
-
 #using first 10
-
 for x in range(0,10):
     category =loandata['fico'][x]     
     if category >= 300 and category < 400:
@@ -109,7 +106,6 @@ for x in range(0,10):
         cat = 'Unknown'     
  
 #applying for loops to all loandata'fico'
-
 ficocat =[]
 length = len(loandata)
 
@@ -134,7 +130,6 @@ ficocat = pd.Series(ficocat)
 loandata['fico.category'] = ficocat
 
 #testing error
-
 ficocat =[]
 length = len(loandata)
 
@@ -167,23 +162,19 @@ loandata['fico.category'] = ficocat
 #df.loc[df[columnName] condition, new column name] = 'value if condition is met
 
 #for interst rate, a new column is wanted. rate> 0.12 (12%) then high, else low(interest rate)
-
 loandata.loc[loandata['int.rate'] > 0.12, 'int.rate.type'] = 'High'
 loandata.loc[loandata['int.rate'] <= 0.12, 'int.rate.type'] = 'Low'
 
 #number of loans/rows by fico.category (poor, etc)
-
 catplot = loandata.groupby(['fico.category']).size()
 catplot.plot.bar(color = 'green', width = 0.1)
 plt.show()
-
 
 purposeCount = loandata.groupby(['purpose']).size()
 catplot.plot.bar(color = 'red', width = 0.2)
 plt.show()
 
 #scatter plots (needs an x and y values of variable)
-
 ypoint = loandata['annualincome']
 xpoint = loandata['dti']
 plt.scatter(xpoint, ypoint, color ='#4caf50')
